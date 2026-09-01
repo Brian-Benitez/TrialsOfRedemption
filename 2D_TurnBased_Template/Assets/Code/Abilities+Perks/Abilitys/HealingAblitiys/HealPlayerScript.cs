987d5ef4f10167ed8acc8ed449df27c24ea40a2b
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2026-09-01 19:17:35 -0400

    Created: Upgrade max health
    Still need to test

--- 2D_TurnBased_Template/Assets/Code/Abilities+Perks/Abilitys/HealingAblitiys/HealPlayerScript.cs
+++ 2D_TurnBased_Template/Assets/Code/Abilities+Perks/Abilitys/HealingAblitiys/HealPlayerScript.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class HealPlayerScript : LevelUpStat
 {
+    private float upgradeHealthAmount;
+    private const float firstLevelHealthUpgrade = 5f;
+    private const float secondLevelHealthUpgrade = 10f;
+    private const float lastLevelHealthUpgrade = 15f;
+    public enum UpgradeTiers
+    {
+        None,
+        FirstUpgrade,
+        SecondUpgrade,
+        LastUpgrade
+    }
+    public UpgradeTiers CurrentTier = UpgradeTiers.None;
     [Header("Scripts")]
     public PlayerInfo PlayerInfoRef;
 
@@ -9,9 +21,11 @@ public class HealPlayerScript : LevelUpStat
     {
         if(PlayerInfoRef.Souls >= CostAmount)
         {
-            PlayerInfoRef.CharacterHealthAmount = PlayerInfoRef.CharacterMaxHealth;
-            //PlayerInfoRef.HealthBarUIRef.SetUIMaxHealth(PlayerInfoRef.CharacterMaxHealth);
-            PlayerInfoRef.SetHealth(PlayerInfoRef.CharacterMaxHealth);
+            StatsLvl++;
+            DetermineCurrentTier();
+            PlayerInfoRef.CharacterMaxHealth += upgradeHealthAmount;//THIS IS FOR UPGRADING HEALTH KEEPING IT HERE FOR LATER USAGE DO NOT DELETE
+            PlayerInfoRef.HealthBarUIRef.SetUIMaxHealth(PlayerInfoRef.CharacterMaxHealth);
+            PlayerInfoRef.SetHealth(PlayerInfoRef.CharacterHealthAmount);
             PlayerInfoRef.Souls -= (int)CostAmount;// if theres issues with souls being subtracted by cost amount its here.
             PlayerInfoRef.UpdatePlayersStats();
             UpdateStatsUI();
@@ -20,13 +34,27 @@ public class HealPlayerScript : LevelUpStat
         {
             Debug.Log("player does not have enough souls.");
         }
-        /*
-        PlayerInfoRef.CharacterMaxHealth += IncrementingStatsAmount;//THIS IS FOR UPGRADING HEALTH KEEPING IT HERE FOR LATER USAGE DO NOT DELETE
-        PlayerInfoRef.HealthBarUIRef.SetUIMaxHealth(PlayerInfoRef.CharacterMaxHealth);
-        PlayerInfoRef.SetHealth(PlayerInfoRef.CharacterHealthAmount);
-        PlayerInfoRef.Souls -= (int)CostAmount;// if theres issues with souls being subtracted by cost amount its here.
-        PlayerInfoRef.UpdatePlayersStats();
-        UpdateStatsUI();
-        */
     }
+
+    void DetermineCurrentTier()
+    {
+        if(StatsLvl == 1)
+            CurrentTier = UpgradeTiers.FirstUpgrade;
+        if (StatsLvl == 2)
+            CurrentTier = UpgradeTiers.SecondUpgrade;
+        if(StatsLvl == 3)
+            CurrentTier = UpgradeTiers.LastUpgrade;
+
+        DetermineHealthAmount();
+    }
+    void DetermineHealthAmount()
+    {
+        if(CurrentTier == UpgradeTiers.FirstUpgrade)
+            upgradeHealthAmount = firstLevelHealthUpgrade;
+        if (CurrentTier == UpgradeTiers.SecondUpgrade)
+            upgradeHealthAmount = secondLevelHealthUpgrade;
+        if(CurrentTier == UpgradeTiers.LastUpgrade)
+            upgradeHealthAmount = lastLevelHealthUpgrade;
+    }
+
 }
